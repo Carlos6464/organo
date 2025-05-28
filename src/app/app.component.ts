88856023ef62bbf5764b3dcccd3e9782b4760ac4
Carlos6464 <adrianosodre23@gmail.com>
2025-05-27 22:08:08 -0300

    Add header and footer components, disable Angular CLI analytics, and update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CabecalhoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
