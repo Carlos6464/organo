0213c39e777128b71c53e0e7cf405a85cb8a75ca
Carlos6464 <adrianosodre23@gmail.com>
2025-05-29 20:58:36 -0300

    Add Livro component with styles and integrate into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LivroComponent } from './components/livro/livro.component';
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, FooterComponent],
+  imports: [CabecalhoComponent, FooterComponent, LivroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
